d82128c55d138247f0a17fd5b30419f63e2dd3c5
Ashish Bishnoi <bishnoi11011@gmail.com>
2026-07-08 11:49:12 +0530

    change zoho script from crmplus to crm

--- app/(normal)/layout.tsx
+++ app/(normal)/layout.tsx
@@ -1,23 +1,24 @@
-import Header from "@/components/commom/header"
-import Footer from "@/components/commom/footer"
-import { getCategories } from "@/helper/category/action"
+import Header from "@/components/commom/header";
+import Footer from "@/components/commom/footer";
 import FloatingWhatsappButton from "@/components/Whatsapp_Button/FloatingWhatsappButton";
 import Script from "next/script";
-import PoperWidget from "@/components/PopupWegit";
 
-export default async function Layout({ children }: {
-    children: React.ReactNode
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-
-    return <>
-        {/* <PoperWidget /> */}
-        <Header />
-        {children}
-        <Script
-            src="https://crmplus.zoho.in/crm/javascript/zcga.js"
-            strategy="afterInteractive"
-        />
-        <Footer />
-        <FloatingWhatsappButton />
+  return (
+    <>
+      {/* <PoperWidget /> */}
+      <Header />
+      {children}
+      <Script
+        type="text/javascript"
+        src="https://crm.zoho.in/crm/javascript/zcga.js"
+      />
+      <Footer />
+      <FloatingWhatsappButton />
     </>
+  );
 }
