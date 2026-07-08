7db748018110f0128a9990f710f74db342da434e
Piyushkhare001 <piyushkharejhs@gmail.com>
2026-07-08 11:15:38 +0530

    Add metadata for blog page

--- app/(normal)/blog/page.tsx
+++ app/(normal)/blog/page.tsx
@@ -1,5 +1,14 @@
 import BlogGridTabs from "@/components/blogs/allBlogsTabs";
 import BlogFeatureHero from "@/components/blogs/banner";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Morzze Blog | Tips & Insights on Kitchen & Bathroom Products",
+  description: "Read the Morzze's Blog for expert tips, trends, and insights on kitchen & bathroom experience. Learn how to enhance your space with style and quality.",
+  alternates: {
+    canonical: "/blog",
+  },
+};
 
 const page = () => {
   return (
